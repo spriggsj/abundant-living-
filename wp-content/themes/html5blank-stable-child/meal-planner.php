7c27d230e4d894334c9001c369f05ecd7c74100e
Roger Chang <Roger Chang>
2016-05-06 16:48:17 -0700

    created meal plan custom post

--- wp-content/themes/html5blank-stable-child/meal-planner.php
+++ wp-content/themes/html5blank-stable-child/meal-planner.php
@@ -2,22 +2,22 @@
 
 	<div class="container">
 		<div class="row main-content">
-			<div class="col-md-12 health">
+			<div class="col-md-12 meal">
 				
 				<?php $loop = new WP_Query( array( 'post_type' => 'health_posts', 'posts_per_page' => 10 ) ); ?> 
 
 				<?php 
 					if (have_posts()) : while (have_posts()) : the_post();
 					
-						echo '<div class="health-title">';
+						echo '<div class="meal-title">';
 							the_title(); 
 						echo'</div>';
 						
-						echo '<div class="col-sm-6 col-md-6 col-center health-img">' ;
+						echo '<div class="col-sm-6 col-md-6 col-center meal-img">' ;
 							the_post_thumbnail();
 						echo '</div>';
 
-						echo '<div class="health-content">';
+						echo '<div class="meal-content">';
 							the_content();
 						echo '</div>';
 
