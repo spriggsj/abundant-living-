0ff2abb7d99e7fc3810093af435c5d6c46cd0d85
jason spriggs <jasonspriggs73@gmail.com>
2016-05-09 15:27:45 -0700

    added front page

---
+++ wp-content/themes/html5blank-stable-child/front-page.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+	
+	<header class="hero-banner">
+		<div class="container">
+			<h1>living <span>healthy</span> should be easy</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod dolores harum fugiat praesentium corrupti illo ducimus, velit non!</p>
+			<input type="submit" value="submit">
+		</div>
+	</header>
+
+<?php get_footer(); ?>
