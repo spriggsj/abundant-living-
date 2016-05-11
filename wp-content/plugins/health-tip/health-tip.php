aec54b3673da80a88ac9b6b7c31d4224bfae78b3
Roger Chang <Roger Chang>
2016-05-10 17:06:20 -0700

    css everything

---
+++ wp-content/plugins/health-tip/health-tip.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+
+	<div class="container">
+		<div class="row main-content">
+			<div class="col-md-12 health-tip">
+				
+				<?php $loop = new WP_Query( array( 'post_type' => 'health_tip_posts', 'posts_per_page' => 10 ) ); ?> 
+
+				<?php 
+					if (have_posts()) : while (have_posts()) : the_post();
+					
+						echo '<div class="health-tip-title">';
+							the_title(); 
+						echo'</div>';
+						
+						echo '<div class="col-sm-6 col-md-6 col-center health-tip-img">' ;
+							the_post_thumbnail();
+						echo '</div>';
+
+						echo '<div class="health-ip-content">';
+							the_content();
+						echo '</div>';
+
+					endwhile; endif;
+
+				?>
+
+			</div>				
+		</div>
+	</div>
+
+<?php get_footer(); ?>
--- wp-content/plugins/health-tip/health_tip_custom_post.php
+++ wp-content/plugins/health-tip/health_tip_custom_post.php
@@ -25,7 +25,7 @@ function rc_health_tip_custom_post(){
 		],
 			'public' => true,
 			'has_archive' => true,
-			'menu_icon' => '',
+			'menu_icon' => 'dashicons-thumbs-up',
 			'rewrite' => array('slug' => 'health tip'),
 			'publicly_queryable' => true,
 			'query_var' => true,
@@ -36,5 +36,60 @@ function rc_health_tip_custom_post(){
 		]
 	);
 }
+
+add_action('admin_init', 'health_tip_post');
+
+function health_tip_post(){
+	add_meta_box(
+		'Health Tip Info',
+		'health_tip_post',
+		'normal',
+		'high'
+	);
+}
+
+function rc_excerpt_length($length){
+	return 35;
+}
+
+add_filter('except_length', 'js_except_length', 999);
+
+function rc_excerpt_more($more){
+	return 'Read More';
+}
+
+add_filter('excerpt_more', 'rc_excerpt_more');
+
+add_action('save_post', 'add_health_tip_fields', 10, 2);
+
+function add_health_tip_fields($health_tip_info_id, $health_tip){
+	if($health_tip->post_type == 'health_tip_post'){
+		if(!current_user_can('edit_post', $health_tip_info_id))
+			return $health_tip_info_id;
+	}
+}
+
+add_filter('template_include', 'include_health_function', 1);
+
+function include_health_tip_function($template_path){
+	if(get_post_type() == 'health_tip_post'){
+		if(is_single()){
+			if($theme_file = locate_template(['health-tip.php'])){
+				$template_path = $theme_file;
+			} else {
+				$template_path = plugin_dir_path(__FILE__) . 'health-tip.php';
+			}
+		}
+	}
+	return $template_path;
+}
+
+function set_post_per_page_health_tip($query){
+	if($query -> is_post_type_archive('health_tip_post')){
+		$query -> set('post_per_page', '8');
+	}
+}
+
+add_action('pre_get_posts', 'set_posts_per_page_for_health');
 
 ?>
