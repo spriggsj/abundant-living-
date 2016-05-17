cee9918bb0dae5e7e878b023d1b7c581f70362b8
Roger Chang <Roger Chang>
2016-05-17 14:46:54 -0700

    styled the footer

--- wp-content/themes/html5blank-stable-child/footer.php
+++ wp-content/themes/html5blank-stable-child/footer.php
@@ -1,29 +1,25 @@
 	<footer>
-		<div class="container footer">
+		<div class="container-fluid footer">
 			<div class="row">
-				<div class="col-xs-12 col-sm-4">
+				<div class="col-xs-12 col-sm-3 wendy-img">
 					<!-- image of wendy maybe -->
 					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/wendy.jpg">
 				</div>
-				<div class="col-xs-12 col-sm-8">
-					<div class="row">
-						<div class="col-xs-12">
-							 <h2><i class="fa fa-heart-o" aria-hidden="true"></i> what you see</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis adipisci suscipit quod quaerat, doloribus quo hic, tempora a rerum, possimus nisi alias illum ipsam sit!</p>
-						</div>
-						<div class="col-xs-12 social-links">
-							<h3>You can find me here</h3>
-
-							<aside>
-								<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-								<a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-								<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-								<a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
-								<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-								<a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-							</aside>
-						</div>
-					</div>
+				<div class="col-xs-12 col-sm-6 footer-about">
+					<h2><span>love</span> what you see</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis adipisci suscipit quod quaerat, doloribus quo hic, tempora a rerum, possimus nisi alias illum ipsam sit!</p>
+					<aside class="social-links">
+						<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+						<a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+						<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+						<a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+						<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+						<a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+						<a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+					</aside>
+				</div>
+				<div class="col-sm-3 footer-logo-img">
+					
 				</div>
 			</div>
 		</div>
