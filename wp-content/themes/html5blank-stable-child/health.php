92bd7a082b66946ee6ef3f3b0e201b01d2229987
jason spriggs <jasonspriggs73@gmail.com>
2016-05-17 16:54:09 -0700

    pinterest

--- wp-content/themes/html5blank-stable-child/health.php
+++ wp-content/themes/html5blank-stable-child/health.php
@@ -7,13 +7,13 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	<div class="health-title">
-		<?php echo the_title() ; ?>/* this displays the title of the post */
+		<?php echo the_title() ; ?> <!--this displays the title of the post -->
 	</div>
 	<div class="col-sm-6 col-md-6 col-center health-img">
-		<?php echo the_post_thumbnail(); ?> /* this is the featured image*/
+		<?php echo the_post_thumbnail(); ?> <!-- this is the featured image-->
 	</div>
 	<div class="health-content">
-		<?php echo the_content(); ?> /* this is the media insert and content */
+		<?php echo the_content(); ?> <!-- this is the media insert and content -->
 	</div>
 
 <?php endwhile; endif; ?>
