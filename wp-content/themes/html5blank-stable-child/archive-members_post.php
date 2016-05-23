078d0bcb3b6b473213ef4dfd769b28dadb328216
Roger Chang <Roger Chang>
2016-05-23 14:34:07 -0700

    fixed members blog page

--- wp-content/themes/html5blank-stable-child/archive-members_post.php
+++ wp-content/themes/html5blank-stable-child/archive-members_post.php
@@ -1,17 +1,15 @@
 <?php get_header(); ?>
 
 	<main role="main">
-		<!-- section -->
-		<section id="red">
-
-			<h2><?php _e( 'Members only', 'html5blank' ); ?></h2>
-
-			<?php get_template_part('loop'); ?>
+		<section class="members-page">
+		
+			<?php 
+				echo do_shortcode( '[member-custom-loop]' );
+			?>
 
 			<?php get_template_part('pagination'); ?>
 
 		</section>
-		<!-- /section -->
 	</main>
 
 <?php get_footer(); ?>
