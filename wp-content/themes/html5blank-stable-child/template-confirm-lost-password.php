03467fb4c0b07d1d4feb15a222153f965bbe125b
Roger Chang <Roger Chang>
2016-06-09 12:11:21 -0700

    finished forgot password page

---
+++ wp-content/themes/html5blank-stable-child/template-confirm-lost-password.php
@@ -0,0 +1,37 @@
+<?php /* Template Name: Confirm Forgot Password Template */ get_header(); ?>
+
+<main role="main">
+		<!-- section -->
+		<section id="confirm-lost-password">
+
+			<h2><?php the_title(); ?></h2>
+
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+			
+			<article class="confirm-password-page">
+				<p>For completing the password reset procedure, please follow the instructions in your email.</p>
+				<?php echo do_shortcode( '[confirm-forgot-password]' ); ?>
+			</article>
+
+		<?php endwhile; ?>
+
+		<?php else: ?>
+
+			<!-- article -->
+			<article>
+
+				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+			</article>
+			<!-- /article -->
+
+		<?php endif; ?>
+
+		</section>
+		<!-- /section -->
+	</main>
+
+
+
+
+<?php get_footer(); ?>
