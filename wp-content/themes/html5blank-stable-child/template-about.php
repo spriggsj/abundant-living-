e5880b784c92ba154c5744a4dc03a9958dfd510d
Roger Chang <Roger Chang>
2016-06-25 13:23:41 -0700

    changed content on about page

--- wp-content/themes/html5blank-stable-child/template-about.php
+++ wp-content/themes/html5blank-stable-child/template-about.php
@@ -15,7 +15,7 @@
 					<p>Wendy Purviance</p>
 				</div>
 				<div class="col-xs-12 col-sm-7 col-md-8 second">
-					<p>Hi Im Wendy Abundant Living Mommy. My journey started 18 years ago. We live in a fast paced world with fast paced foods. You can go to any store and just about every meal you can imagine is available frozen. How about the fast food restaurants on every corner? Delivery anyone? or maybe you run into the store and pick up something under the heat lamp.</p>
+					<p>Hi I'm Wendy, from Abundant Living Mommy. My journey started 18 years ago. We live in a fast paced world with fast paced foods. You can go to any grocery store and just about every meal you can imagine is available frozen. How about the fast food restaurants on every corner? Delivery anyone? Maybe you run into the store and pick up something under the heat lamp.</p>
 					<p>It's easy to see how this can happen. We work hard, fill our days from sun up to well beyond sun down, dragging ourselves to bed only to start it all over again a few hours later.</p>
 				</div>
 			</div>
@@ -23,15 +23,15 @@
 	</section>
 	<section class="about-wendy">
 		<div class="container">
-			<p>1 in 2 Americans will get cancer. 36% of Americans are over weight. 34% Obese, which doubles the risk of heart disease, stroke along with Type 2 diabetes. It doesn't stop there. Alarming numbers of Americans are suffering Autoimmune disease, depress, anxiety, hormone problems, ADHD, fertility and many more. The last two decades my husband and I have changed our diet, to many times to count. We changed after his father had open heart surgery, a low fat diet. After his mother was diagnosed with cancer. When we wanted to lose weight. When I had fertility issues and later while pregnant diagnosed with gestational diabetes. After my husband developed high blood pressure and I found out I have an autoimmune, raised cholesterol and leaking gut. Whew............</p>
+			<p>1 in 2 Americans will get cancer. 36% of Americans are over weight. 34% Obese, this doubles the risk of heart disease, stroke, and Type 2 diabetes. It doesn't stop there. Alarming numbers of Americans are suffering autoimmune diseases, depression, anxiety, hormone problems, ADHD, fertility and many more. The last two decades my husband and I have changed our diet, too many times to count. We changed after his father had open heart surgery, to a low fat diet, after his mother was diagnosed with cancer, and when we wanted to lose weight. My diet also changed during pregnancy after I was diagnosed with gestational diabetes.  My husband has been battling high blood pressure and recently I found out I have an autoimmune disorder, raised cholesterol and leaking gut. Whew............</p>
 		</div>
 	</section>
 	<section class="about-wendy second-about-me">
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-8 third">
-					<p>However, in all these diet changes and transformations our goal was always to eat the healthiest we knew how. This is a real challenge in the world I described earlier. After the birth of our little miracle baby girl, anxiety overcame me. The gestational diabetes put my little girl at an increased risk of developing diabetes herself. In addition to all the hereditary diseases in our family. I had a C-Section(not planned) so she didn't receive any Flora. Which helps start supporting her immune system. I started to research more and more about the foods we eat and the toxic world we live in. This encompasses our food, environment, homes and even healthcare. I started to educated myself and became an expert at reading labels and knowing about the ingredients in our food.</p>
-					<p>I was shocked at what was in even organic foods. An ingredient like carrageen- which used as a thinking agent, found in milk, cottage cheese, dog food, meats and even cosmetics, just to name a few. Carrageen is a gut irritant causing inflammation and possibly cancer. At home I started looking at chemicals we use daily. Cleaners, dusting agents, weed killers and cookware. We all know the effects of BPA, but there are so many more unfriendly, harmful chemicals in our products causing a toxic environment.</p>
+					<p>However, in all these diet changes and transformations our goal was always to eat the healthiest that we could. This is a real challenge in the world I described earlier. After the birth of our little miracle baby girl, anxiety overcame me. The gestational diabetes put my little girl at an increased risk of developing diabetes herself. In addition to all the hereditary diseases in our family, I had a C-Section(not planned) so she didn't receive any Flora. This helps start supporting her immune system. I started to research more and more about the foods we eat and the toxic world we live in.  The research that I found effects our food, environment, homes and even healthcare.  I started to educate myself and bacame an expert at reading labels and knowing about the ingredients in out food.</p>
+					<p>I was shocked at what was in even organic foods. An ingredient like carrageen- which is used as a thinking agent is found in milk, cottage cheese, dog food, meats and even cosmetics.  Carrageen is a gut irritant causing inflammation and possibly cancer. At home I started looking at chemicals we use daily. Cleaners, dusting agents, weed killers and cookware. We all know the effects of BPA, but there are so many more unfriendly, harmful chemicals in our products causing a toxic environment.</p>
 				</div>
 				<div class="col-sm-4 fourth">
 					
@@ -52,23 +52,23 @@
 				</div>
 				<div class="col-sm-8 sixth">
 					<p>Kids toys you would think they would be safe right? Wrong! Many of the toys are imported from places like China with very poor regulations. These are the toys our children put in their mouths, lay on and play with.</p>
-					<p>My quest started. I cant control every aspect of my families life. But, my goal is to try my best to create a toxic free environment and have the healthiest foods we can.</p>
-					<p>I invite you to join me on this journey. I am a real mom with the same challenges you have. I have a messy, imperfect life. But everyday we are given we have another opportunity to change, grow, transform ourselves and families. As woman we are the center of the home and have the opportunity to mold and teach. We have power in numbers. Id love to get to know you so we can encourage each other. As a wellness coach and mommy Ive put together simple raw, healthy meals, toxic free tips for your home and family. Lets do Life together.</p>
+					<p>My quest started. I can't control every aspect of my families life. But, my goal is to try my best to create a toxic free environment and have the healthiest foods we can.</p>
+					<p>I invite you to join me on this journey. I am a real mom with the same challenges you have. I have a messy, imperfect life. But everyday we are given, we have another opportunity to change, grow, transform ourselves and families. As woman we are the center of the home and have the opportunity to mold and teach. We have power in numbers. I would love to get to know you so we can encourage each other. As a wellness coach and mommy I have put together simple raw, healthy meals, as well as toxic free tips for your home and family. Lets do life together.</p>
 				</div>
 			</div>
 		</div>
 	</section>
 	<section class="about-wendy random-things">
 		<div class="container">
-			<h2>A few random things to get to know me better</h2>
+			<h2>A few random things to get to know me better.</h2>
 			<ul>
 				<li>I am married to the love of my life and knew he was the one on our first date.</li>
-				<li>I love being a stay at home wife and mommy. I Couldn't bear to leave my little girl everyday.</li>
+				<li>I love being a stay at home wife and mommy. I couldn't bear to leave my little girl everyday.</li>
 				<li>My greatest joy is being a mommy to our 2 1/2 year old little girl.</li>
 				<li>Yoga is my happy place. It literally changed my life.</li>
-				<li>I absolutely love college football. We never miss college game day on Saturday mornings. We tape them to reply during the off season when we are missing football.</li>
+				<li>I absolutely love college football. We never miss ESPN college game day on Saturday mornings. We tape them to replay during the off season when we are missing football.</li>
 				<li>I am a early riser. Enjoying the quiet and stillness of the morning. Prepping for my families day.</li>
-				<li>I love to read a good book, although I haven't had as much time for this since having my daughter and my book pile is piling up.</li>
+				<li>I love to read a good book, although I haven't had as much time for this since having my daughter.</li>
 				<li>I Love Jesus and sharing his word and power. I couldn't do this LIFE without HIM.</li>
 			</ul>
 		</div>
